813cccbf827fc53fba6b6a03c18ed24e5e30740d
Walace Andrade Matos <walace.andr.m@gmail.com>
2023-09-24 15:37:41 -0300

    Added states for CalculatorForm input fields.

--- src/components/CalculatorForm.js
+++ src/components/CalculatorForm.js
@@ -1,14 +1,56 @@
+import { useState } from "react";
+
 const CalculatorForm = () => {
+  const [currentSavings, setCurrentSavings] = useState("");
+  const [yearlySavings, setYearlySavings] = useState("");
+  const [expectedInterest, setExpectedInterest] = useState("");
+  const [investmentDuration, setInvestmentDuration] = useState("");
+
+  const currentSavingsChangeHandler = (event) => {
+    setCurrentSavings(event.target.value);
+  };
+
+  const yearlySavingsChangeHandler = (event) => {
+    setYearlySavings(event.target.value);
+  };
+
+  const expectedInterestChangeHandler = (event) => {
+    setExpectedInterest(event.target.value);
+  };
+
+  const investmentDurationChangeHandler = (event) => {
+    setInvestmentDuration(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    setCurrentSavings("");
+    setYearlySavings("");
+    setExpectedInterest("");
+    setInvestmentDuration("");
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            value={currentSavings}
+            onChange={currentSavingsChangeHandler}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={yearlySavings}
+            onChange={yearlySavingsChangeHandler}
+          />
         </p>
       </div>
       <div className="input-group">
@@ -16,11 +58,21 @@ const CalculatorForm = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            value={expectedInterest}
+            onChange={expectedInterestChangeHandler}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            value={investmentDuration}
+            onChange={investmentDurationChangeHandler}
+          />
         </p>
       </div>
       <p className="actions">
